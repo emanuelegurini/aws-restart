27f7b8ec9c8567937696239538da94c0435d3876
emanuelegurini <emanuele.gurini89@gmail.com>
2025-12-12 20:51:32 +0100

    refactor: error hadling

--- fake-store/main.py
+++ fake-store/main.py
@@ -1,5 +1,5 @@
 from requests import get, post, Response
-from requests.exceptions import HTTPError, ConnectionError, Timeout, RequestException
+from requests.exceptions import HTTPError, ConnectionError, Timeout
 
 BASE_URL: str = "https://api.escuelajs.co/api/v1/products"
 
@@ -8,50 +8,39 @@ BASE_URL: str = "https://api.escuelajs.co/api/v1/products"
 # ===============================
 
 def get_lista_prodotti(URL: str) -> list[dict[str, any]]:
-    if URL is None: 
+    if not URL: 
         raise ValueError("L'URL non può essere vuoto!")
 
-    try: 
-        response: Response = get_data(URL) 
-        data = response.json()
+    response: Response = get_data(URL) 
+    data = response.json()
 
-        if not isinstance(data, list):
-            raise TypeError(
-                f"Risposta inattesa: mi aspettavo un lista, "
-                f"ma ho ricevuto {type(data).__name__}"
-        )
+    if not isinstance(data, list):
+        raise TypeError(
+            f"Risposta inattesa: mi aspettavo un lista, "
+            f"ma ho ricevuto {type(data).__name__}"
+    )
 
-        return data
-    except Exception as e:
-        raise Exception(f"Problema con la response: {e}")
+    return data
 
 def get_prodotto(URL: str) -> dict[str, any]:
-    if URL is None: 
+    if not URL: 
         raise ValueError("L'URL non può essere vuoto!")
 
-    try: 
-        response: Response = get_data(URL) 
-        data = response.json()
+    response: Response = get_data(URL) 
+    data = response.json()
 
-        if not isinstance(data, dict):
-            raise TypeError(
-                f"Risposta inattesa: mi aspettavo un dict, "
-                f"ma ho ricevuto {type(data).__name__}"
-        )
+    if not isinstance(data, dict):
+        raise TypeError(
+            f"Risposta inattesa: mi aspettavo un dict, "
+            f"ma ho ricevuto {type(data).__name__}"
+    )
 
-        return data
-
-    except Exception as e:
-        raise Exception(f"Problema con la response: {e}")
-
+    return data
 
 def create_product(URL: str, data: dict) -> dict[str, any]:
 
-    if URL is None: 
+    if not URL or not data: 
         raise ValueError("L'URL non può essere vuoto!")
-
-    if data is None: 
-        raise ValueError("Data non può essere vuoto!")
 
     if not isinstance(data, dict):
         raise TypeError(
@@ -59,87 +48,39 @@ def create_product(URL: str, data: dict) -> dict[str, any]:
             f"ma ho ricevuto {type(data).__name__}"
         )
 
-    try:
-        response = post_data(URL, data)
-        return response.json()
-
-    except Exception as e:
-        raise Exception(f"Problema con la response: {e}")
-
+    response = post_data(URL, data)
+    return response.json()
 
 def create_category(URL: str, data: dict) -> dict[str, any]:
 
-    if URL is None: 
+    if not URL or not data: 
         raise ValueError("L'URL non può essere vuoto!")
-
-    if data is None: 
-        raise ValueError("Data non può essere vuoto!")
 
     if not isinstance(data, dict):
         raise TypeError(
             f"Risposta inattesa: mi aspettavo un dict, "
             f"ma ho ricevuto {type(data).__name__}"
-        )
+    	)
 
-    try:
-        response = post_data(URL, data)
-        return response.json()
-
-    except Exception as e:
-        raise Exception(f"Problema con la response: {e}")
+    response = post_data(URL, data)
+    return response.json()
  
 
 def post_data(URL: str, data: dict) -> Response:
-    try: 
-        response = post(URL, headers={"Content-Type": "application/json"}, json=data)
-        print(response)
-        response.raise_for_status()
+    if not URL: 
+        raise ValueError("L'URL non può essere vuoto!")
 
-        return response
-
-    except HTTPError as e:
-        r = e.response 
-        details = None
-        if r is not None:
-            try:
-                details = r.json()   
-            except Exception:
-                details = r.text    
-        raise HTTPError(
-            f"Errore HTTP {(r.status_code if r else 'N/A')} su {URL}: "
-            f"{(r.reason if r else '')} - Dettagli: {details}"
-        ) from e 
-
-    except ConnectionError:
-        raise ConnectionError(f"Impossibile connettersi a {URL}")
-    
-    except Timeout:
-        raise Timeout(f"Timeout nella richiesta a {URL}")
-    
-    except RequestException as e:
-        raise RequestException(f"Errore di rete imprevisto: {e}") from e
+    response: Response = post(URL, headers={"Content-Type": "application/json"}, json=data)
+    response.raise_for_status()
+    return response
 
 def get_data(URL: str) -> Response:
-    if URL is None: 
+    if not URL: 
         raise ValueError("L'URL non può essere vuoto!")
     
-    try:
-        response = get(URL)
-        response.raise_for_status()
-        return response
-
-    except HTTPError as e:
-        raise HTTPError(f"Errore HTTP {response.status_code} su {URL}: {response.reason}"
-        ) from e
-
-    except ConnectionError:
-        raise ConnectionError(f"Impossibile connettersi a {URL}")
-    
-    except Timeout:
-        raise Timeout(f"Timeout nella richiesta a {URL}")
-    
-    except RequestException as e:
-        raise RequestException(f"Errore di rete imprevisto: {e}") from e
+    response : Response = get(URL)
+    response.raise_for_status()
+    return response
 
 # ===============================
 #   Model
@@ -166,10 +107,10 @@ def print_prodotto(product: dict[str, any]) -> None:
     print("*" * 30)
     print("PRODOTTO")
     print("*" * 30)
-    print(f"ID: {product["id"]}")
-    print(f"Titolo: {product["title"]}")
-    print(f"Category: {product["category"]["name"]}")
-    print(f"PRICE: {product["price"]}")
+    print(f"ID: {product['id']}")
+    print(f"Titolo: {product['title']}")
+    print(f"Category: {product['category']['name']}")
+    print(f"PRICE: {product['price']}")
 
 def print_product_list(product_list: list[dict]) -> None:
     print("*" * 30)
@@ -177,10 +118,10 @@ def print_product_list(product_list: list[dict]) -> None:
     print("*" * 30)
 
     for product in product_list:
-        print(f"{product["id"]} - {product["title"]}")
+        print(f"{product['id']} - {product['title']}")
 
-product = {
-  "title": "Pippos and friends",
+prod = {
+  "title": "Pippo tanto",
   "price": 10,
   "description": "A description",
   "categoryId": 7,
@@ -195,17 +136,18 @@ def main() -> None:
         # product= product_model(get_prodotto(f"{BASE_URL}/{id}"))
         #print_prodotto(product)
 
-        print_prodotto(create_product(BASE_URL, product))
+        print_prodotto(create_product(BASE_URL, prod))
     
+    except HTTPError as e:
+        print(f"Errore API: {e}")
+    except ConnectionError:
+        print("Impossibile connettersi al server")
+    except Timeout:
+        print("Il server non risponde")
     except ValueError as e:
-        print(f"{e}")
-    
-    except FileNotFoundError as e:
-        print(f"{e}")
-    
-    except Exception as e:
-        print(f"{e}") 
-
+        print(f"Dati non validi: {e}")
+    except TypeError as e:
+        print(f"Tipo di dato inatteso: {e}")
     
 
 if __name__ == "__main__":
