b0c48c44aa0d031e0c83d1a2b53fca0a49ad2e5c
emanuelegurini <emanuele.gurini89@gmail.com>
2025-12-18 15:09:59 +0100

    feat: add fetch users

--- whounfollow-api/main.py
+++ whounfollow-api/main.py
@@ -1,73 +1,47 @@
-DATA = [
-  {
-    "login": "lmammino",
-    "id": 205629,
-    "node_id": "MDQ6VXNlcjIwNTYyOQ==",
-    "avatar_url": "https://avatars.githubusercontent.com/u/205629?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/lmammino",
-    "html_url": "https://github.com/lmammino",
-    "followers_url": "https://api.github.com/users/lmammino/followers",
-    "following_url": "https://api.github.com/users/lmammino/following{/other_user}",
-    "gists_url": "https://api.github.com/users/lmammino/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/lmammino/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/lmammino/subscriptions",
-    "organizations_url": "https://api.github.com/users/lmammino/orgs",
-    "repos_url": "https://api.github.com/users/lmammino/repos",
-    "events_url": "https://api.github.com/users/lmammino/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/lmammino/received_events",
-    "type": "User",
-    "user_view_type": "public",
-    "site_admin": False
-  },
-  {
-    "login": "luduvigo",
-    "id": 1569913,
-    "node_id": "MDQ6VXNlcjE1Njk5MTM=",
-    "avatar_url": "https://avatars.githubusercontent.com/u/1569913?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/luduvigo",
-    "html_url": "https://github.com/luduvigo",
-    "followers_url": "https://api.github.com/users/luduvigo/followers",
-    "following_url": "https://api.github.com/users/luduvigo/following{/other_user}",
-    "gists_url": "https://api.github.com/users/luduvigo/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/luduvigo/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/luduvigo/subscriptions",
-    "organizations_url": "https://api.github.com/users/luduvigo/orgs",
-    "repos_url": "https://api.github.com/users/luduvigo/repos",
-    "events_url": "https://api.github.com/users/luduvigo/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/luduvigo/received_events",
-    "type": "User",
-    "user_view_type": "public",
-    "site_admin": False
-  },
-  {
-    "login": "pfuhrmann",
-    "id": 1627445,
-    "node_id": "MDQ6VXNlcjE2Mjc0NDU=",
-    "avatar_url": "https://avatars.githubusercontent.com/u/1627445?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/pfuhrmann",
-    "html_url": "https://github.com/pfuhrmann",
-    "followers_url": "https://api.github.com/users/pfuhrmann/followers",
-    "following_url": "https://api.github.com/users/pfuhrmann/following{/other_user}",
-    "gists_url": "https://api.github.com/users/pfuhrmann/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/pfuhrmann/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/pfuhrmann/subscriptions",
-    "organizations_url": "https://api.github.com/users/pfuhrmann/orgs",
-    "repos_url": "https://api.github.com/users/pfuhrmann/repos",
-    "events_url": "https://api.github.com/users/pfuhrmann/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/pfuhrmann/received_events",
-    "type": "User",
-    "user_view_type": "public",
-    "site_admin": False
-  }
-]
-
 import datetime
 import uuid
 import os
 import json
+from requests import get, Response
+from dotenv import load_dotenv
+
+load_dotenv()
+
+base_url = os.getenv("BASE_URL")
+github_token = os.getenv("GITHUB_TOKEN")
+
+def get_all_follower_from_pages(username: str) -> list[dict]:
+    """Prende tutte le pagine con i follower e ne restituisce la lista accorpata"""
+    url = f"{base_url}/users/{username}/followers"
+    page: int = 1
+    users: list = []
+
+    while True:
+        print(f"Sto contattando pagina: {page}")
+        response = fetch_users(url, page)
+
+        users.extend(response.json())
+
+        if not has_next_page(response):
+            break
+
+        page = page + 1
+    
+    return users
+
+def has_next_page(response: Response) -> bool:
+    "Verifica che esista un'altra pagina per prendere i follower"
+    link_header = response.headers.get("Link", "")
+    return "next" in link_header
+
+
+def fetch_users(url: str, page: int) -> Response:
+    """Esegue la chiamata per prendere tutti i dati dal server"""
+    headers = {
+            "Authorization": f"Bearer {github_token}",
+            "X-GitHub-Api-Version": "2022-11-28"
+        }
+    return get(f"{url}?page={page}", headers=headers)
 
 def extract_usernames(users: list[dict]) -> list[str]:
     usernames: list[str] = []
@@ -124,14 +98,17 @@ def save_json_db(db_name: str, new_value: dict) -> None:
 
 def main() -> None:
     print("Inizio programma")
-
+    """
     lista_test = extract_usernames(DATA)
 
     record = create_record(lista_test)
     
     save_json_db("db/db.json", record)
     print("fine programma")
+    """
 
+    data = get_all_follower_from_pages("emanuelegurini")
+    print(extract_usernames(data))
 
 if __name__ == "__main__":
     main()
