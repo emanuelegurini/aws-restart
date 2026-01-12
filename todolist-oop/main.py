1fa61bbcd782f44abe8fbc7c88fc823527839249
emanuelegurini <emanuele.gurini89@gmail.com>
2026-01-12 14:16:38 +0100

    split classes

--- todolist-oop/main.py
+++ todolist-oop/main.py
@@ -1,64 +1,6 @@
-"""
-- menu
-- todolist
-- project
-- task
-- tag
-"""
-
-class Menu:
-    def printMenu(self) -> None:
-        print(f"""
-        1. Add Project
-        2. Add Task
-        3. Add Tag
-        4. List Pojects
-        5. List Task
-        6. List Tags
-        7. exit
-    """)
-
-class Todolist:
-    def __init__(self):
-        self.projects = []
-
-    def add_project(self, project: Project) -> None:
-        self.projects.append(project)
-    
-    def get_projects_lenght(self) -> int:
-        return len(self.projects)
-
-    def get_projects(self) -> list[Project]:
-        for p in self.projects:
-            return f"{p.id} - {p.name}"
-        
-    def update_project_name(self, id: str) -> None:
-        target = next((project for project in self.projects if project.get_project_id() == id), None)
-        target.set_project_name("pippo")
-
-
-import uuid
-
-class Project:
-    def __init__(self, name: str):
-        self.id = str(uuid.uuid4()) 
-        self.name = name
-        self.task_list = []
-
-    def get_project_id(self):
-        return self.id
-    
-    def get_tasks_lenght(self) -> int:
-        return len(self.task_list)
-
-    def get_project_name(self) -> str:
-        return self.name
-
-    def set_project_name(self, new_name:str) -> None:
-        self.name = new_name
-
-
-
+from todolist import Todolist
+from project import Project
+from menu import Menu
 
 def main():
     todolist = Todolist()
---
+++ todolist-oop/menu.py
@@ -0,0 +1,11 @@
+class Menu:
+    def printMenu(self) -> None:
+        print(f"""
+    1. Add Project
+    2. Add Task
+    3. Add Tag
+    4. List Pojects
+    5. List Task
+    6. List Tags
+    7. exit
+    """)
