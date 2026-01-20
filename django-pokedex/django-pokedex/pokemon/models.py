d111edd523071c51525f1fbc8f27ea44e65d406a
emanuelegurini <emanuele.gurini89@gmail.com>
2026-01-20 14:04:50 +0100

    add pokedex files

---
+++ django-pokedex/django-pokedex/pokemon/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class Pokemon(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    pokedex_id = models.IntegerField()
+    
---
+++ django-pokedex/django-pokedex/pokemon/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Pokemon
+from django.http import JsonResponse
+
+# Create your views here.
+
+def add_pokemon(request):
+    pokemon = Pokemon.objects.create(name="Bulbasaur", pokedex_id=1)
+    return JsonResponse({'id': pokemon.id, 'name': pokemon.name, 'pokedex_id': pokemon.pokedex_id})
