e0b7343f6d240f78206a5271a13101797302fddf
emanuelegurini <emanuele.gurini89@gmail.com>
2026-01-22 12:03:20 +0100

    feat: add put and patch

--- django-pokedex/django-pokedex/pokemon/urls.py
+++ django-pokedex/django-pokedex/pokemon/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.get_pokemon_list, name="get_pokemon_list"),
-    path("add", views.add_pokemon, name="add_pokemon"), 
+    path("", views.add_pokemon, name="add_pokemon"),
+    path("/delete/<str:id>", views.delete_pokemon, name="delete_pokemon"), 
+    path("/update/patch/<str:id>", views.update_patch_pokemon, name="update_patch_pokemon"), 
+    path("/update/put/<str:id>", views.update_put_pokemon, name="update_put_pokemon"), 
 ]
--- django-pokedex/django-pokedex/pokemon/views.py
+++ django-pokedex/django-pokedex/pokemon/views.py
@@ -2,11 +2,25 @@ from django.db import IntegrityError, OperationalError
 from django.shortcuts import render
 from .models import Pokemon
 from django.http import JsonResponse
-from django.views.decorators.http import require_POST, require_GET
+from django.views.decorators.http import require_POST, require_GET, require_http_methods
 import json
 from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
+@require_GET
+def get_pokemon_list(request):
+    try:
+        pokemon_list = list(Pokemon.objects.all().values())
+        return JsonResponse(pokemon_list, safe=False)
+    
+    except OperationalError:
+        # Problemi di connessione al database
+        return JsonResponse({'error': 'Database non disponibile'}, status=503)
+    
+    except Exception as e:
+        # Errore generico imprevisto
+        return JsonResponse({'error': str(e)}, status=500)
+    
 @csrf_exempt 
 @require_POST
 def add_pokemon(request):
@@ -42,16 +56,62 @@ def add_pokemon(request):
     except Exception as e:
         return JsonResponse({'error': 'Errore interno del server'}, status=500)
 
-@require_GET
-def get_pokemon_list(request):
+@csrf_exempt
+@require_http_methods(['DELETE'])
+def delete_pokemon(request, id):
     try:
-        pokemon_list = list(Pokemon.objects.all().values())
-        return JsonResponse(pokemon_list, safe=False)
+        pokemon = Pokemon.objects.get(id=id)
+        pokemon.delete()
+        return JsonResponse({'message': 'Pokemon eliminato'}, status=200)
+    except Pokemon.DoesNotExist:
+        return JsonResponse({'error': 'Pokemon non trovato'}, status=404)
     
-    except OperationalError:
-        # Problemi di connessione al database
-        return JsonResponse({'error': 'Database non disponibile'}, status=503)
+@csrf_exempt
+@require_http_methods(['PATCH'])
+def update_patch_pokemon(request, id):
+    try:
+        data = json.loads(request.body)
+        pokemon = Pokemon.objects.get(id=id)
+        
+        # Aggiorna solo i campi presenti nel body
+        if 'name' in data:
+            pokemon.name = data['name']
+        
+        pokemon.save()
+        
+        return JsonResponse({
+            'id': pokemon.id,
+            'name': pokemon.name,
+            'pokedex_id': pokemon.pokedex_id
+        }, status=200)
     
-    except Exception as e:
-        # Errore generico imprevisto
-        return JsonResponse({'error': str(e)}, status=500)
+    except Pokemon.DoesNotExist:
+        return JsonResponse({'error': 'Pokemon non trovato'}, status=404)
+    
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'JSON non valido'}, status=400)
+
+@csrf_exempt
+@require_http_methods(['PUT'])
+def update_put_pokemon(request, id):
+    try:
+        data = json.loads(request.body)
+        pokemon = Pokemon.objects.get(id=id)
+        
+        
+        pokemon.name = data['name']
+        pokemon.pokedex_id = data['pokedex_id']
+        
+        pokemon.save()
+        
+        return JsonResponse({
+            'id': pokemon.id,
+            'name': pokemon.name,
+            'pokedex_id': pokemon.pokedex_id
+        }, status=200)
+    
+    except Pokemon.DoesNotExist:
+        return JsonResponse({'error': 'Pokemon non trovato'}, status=404)
+    
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'JSON non valido'}, status=400)
