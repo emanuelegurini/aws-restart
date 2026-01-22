ce4c6bb71de94a9d39e73854a9577e4d38d3d18d
emanuelegurini <emanuele.gurini89@gmail.com>
2026-01-22 14:23:52 +0100

    add migrations

--- django-pokedex/django-pokedex/pokedex/settings.py
+++ django-pokedex/django-pokedex/pokedex/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pokemon',
+    'allenatore',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django-pokedex/django-pokedex/pokemon/models.py
+++ django-pokedex/django-pokedex/pokemon/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from allenatore.models import Allenatore
 
 # Create your models here.
 
@@ -7,4 +8,7 @@ class Pokemon(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200, unique=True)
     pokedex_id = models.IntegerField()
+
+    allenatore = models.ForeignKey(Allenatore, on_delete=models.CASCADE, related_name="pokemon", null=True, blank=True)
+
     
